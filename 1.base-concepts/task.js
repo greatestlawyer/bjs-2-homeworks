298e9c641557a936939cbf4e7bdebda67563e7e3
Артур Филонов <filonov.artur@bk.ru>
2022-07-26 14:36:17 +0300

    new changes

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,21 +2,17 @@
 
 
 function solveEquation(a, b, c) {
-  let root1;
-  let root2;
   let arr = [];
   let discriminant = b ** 2 - 4 * a * c;
   console.log(discriminant);
   
   if (discriminant === 0) {
     arr.push(-b / (2 * a));
-    //console.log(`${root1} и ${root2}`);
-    //console.log(arr.push(`${root1} и ${root2}`));
+    
 } else if (discriminant > 0) {
     arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
     arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
-    //console.log(`${root1} и ${root2}`);
-    //console.log(arr.push(`${root1} и ${root2}`));
+  
 }
   return arr; 
 }
