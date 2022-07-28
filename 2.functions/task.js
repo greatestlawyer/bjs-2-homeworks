5f8bb49216fa9721d910e832e0a02cf2e4178d1f
Артур Филонов <filonov.artur@bk.ru>
2022-07-28 08:47:56 +0300

    2 task

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,63 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = Infinity; 
+  let max = -Infinity; 
+  let sum = 0;
+  let avg;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i]; 
+    } if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+    avg = sum/(Number(arr.length.toFixed(2));
+  }
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
   let max;
-
-  // Ваш кода
-  // for ...
   
+  
+  for (let i = 0; i < arrOfArr.length; i++) {
+    max += arrOfArr[i];
+  }
+
+  if (func(arrOfArr[i] > max) {
+    func(arrOfArr[i]) = max;
+  }
+  
+
   return max;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
 // Задание 3
-function worker2(arr) {
+//function worker2(arr) {
   // Ваш код
-}
+//}
