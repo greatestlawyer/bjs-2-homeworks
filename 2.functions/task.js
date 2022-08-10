c35b40d87f70952dbf35e1dd5fd7f9cba3e025f3
Артур Филонов <filonov.artur@bk.ru>
2022-08-10 09:48:37 +0300

    new changes

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -8,19 +8,43 @@ function getArrayParams(arr) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > max) {
       max = arr[i]; 
+
+    } 
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+
     } if (arr[i] < min) {
       min = arr[i];
     }
     sum += arr[i];
     avg = sum/(Number(arr.length.toFixed(2));
+
   }
 
+  avg = sum/(Number(arr.length.toFixed(2));
+  
   return { min: min, max: max, avg: avg };
 }
+
+
+
+
+
+// Задание 2
+
+arrOfArr=[[1, 2, 3, 4], [10, 20, -10, -20]];
+ 
+
+function worker(arr) {
+  let sum = 0;
+
 
 // Задание 2
 function worker(arr) {
   let sum = 0;
+
 
   for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
@@ -32,6 +56,18 @@ function worker(arr) {
 function makeWork(arrOfArr, func) {
   let max;
   
+
+  for (let i = 0; i < arrOfArr.length; i++) {
+    if (func(arrOfArr[i] > max)) {
+    max = func(arrOfArr[i]);
+  }
+  }
+
+  return max;
+}
+
+makeWork(arrOfArr,worker);
+
   
   for (let i = 0; i < arrOfArr.length; i++) {
     max += arrOfArr[i];
@@ -47,17 +83,17 @@ function makeWork(arrOfArr, func) {
 
 
 
-
-
-
-
-
-
-
-
-
-
 // Задание 3
-//function worker2(arr) {
-  // Ваш код
-//}
+
+function worker2(arr) {
+let min = Math.min(...arr);
+let max = Math.max(...arr);
+let distance = Math.abs(min - max);
+console.log(distance);
+}
+worker2([1,15,567,55,777]);
+
+
+makeWork(arrOfArr,worker2);
+
+
