44816ee65429ce1ed4202bbf30fbcf95756d5845
Артур Филонов <filonov.artur@bk.ru>
2022-08-11 08:08:53 +0300

    add changes

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,14 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  let result = arr1.every((i, index) => i === arr2[index]);
+  
+  let result = (arr1.length == arr2.length) && arr1.every((i, index) => i === arr2[index]);
+  
 
   return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
   
   let result = arr.filter((number) => number > 0 ).filter((number) => number % 3 == 0).map((number) => number * 10);
  
-  return resultArr;
+  return result;
 }
