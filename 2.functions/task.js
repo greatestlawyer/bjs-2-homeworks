cd3fe2a8b82c19c3f8e827d0319a194ec8321c80
Артур Филонов <filonov.artur@bk.ru>
2022-08-12 08:50:43 +0300

    added new changes

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -3,42 +3,23 @@ function getArrayParams(arr) {
   let min = Infinity; 
   let max = -Infinity; 
   let sum = 0;
-  let avg;
+  let avg = Number((sum/arr.length).toFixed(2));
 
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > max) {
-      max = arr[i]; 
-
-    } 
+      max = arr[i]
+    }
     if (arr[i] < min) {
-      min = arr[i];
+      min = arr[i]
     }
+    
     sum += arr[i];
-
-    } if (arr[i] < min) {
-      min = arr[i];
-    }
-    sum += arr[i];
-    avg = sum/(Number(arr.length.toFixed(2));
-
   }
 
-  avg = sum/(Number(arr.length.toFixed(2));
   
   return { min: min, max: max, avg: avg };
 }
 
-
-
-
-
-// Задание 2
-
-arrOfArr=[[1, 2, 3, 4], [10, 20, -10, -20]];
- 
-
-function worker(arr) {
-  let sum = 0;
 
 
 // Задание 2
@@ -53,12 +34,13 @@ function worker(arr) {
   return sum;
 }
 
+
 function makeWork(arrOfArr, func) {
   let max;
   
 
   for (let i = 0; i < arrOfArr.length; i++) {
-    if (func(arrOfArr[i] > max)) {
+    if (func(arrOfArr[i]) > max) {
     max = func(arrOfArr[i]);
   }
   }
@@ -69,19 +51,6 @@ function makeWork(arrOfArr, func) {
 makeWork(arrOfArr,worker);
 
   
-  for (let i = 0; i < arrOfArr.length; i++) {
-    max += arrOfArr[i];
-  }
-
-  if (func(arrOfArr[i] > max) {
-    func(arrOfArr[i]) = max;
-  }
-  
-
-  return max;
-}
-
-
 
 // Задание 3
 
@@ -89,11 +58,10 @@ function worker2(arr) {
 let min = Math.min(...arr);
 let max = Math.max(...arr);
 let distance = Math.abs(min - max);
-console.log(distance);
+
+return distance;
 }
-worker2([1,15,567,55,777]);
 
 
-makeWork(arrOfArr,worker2);
 
 
