6a046c8445d20a895eb072dc0955e71814345e4b
Артур Филонов <filonov.artur@bk.ru>
2022-08-17 08:54:35 +0300

    added task for the fisrt time

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,47 @@
-function Student(name, gender, age) {
-    // Ваш код
-
+const Student = function(
+  name,
+  gender,
+  age
+){
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+const student1 = new Student(
+  'Alex','male', 34
+);
+const student2 = new Student(
+  'Android','male', 24
+); //1.Создал функцию конструктор Student(name, gender, age) и с помощью оператора new несколько экземпляров объектов (студентов).
 
-// ваш код для остальных методов
+Student.prototype.setSubject = function setSubject(subjectName) {
+  this.subject = subjectName;
+} //2.Создал доступный для всех экземпляров student метод setSubject(subjectName), который при вызове устанавливает поле предмет subject экземпляра в subjectName. 
+
+Student.prototype.addMark = function addMark(mark) {
+  if(this.marks === undefined){ 
+    this.marks = [];
+    this.marks.push(mark);
+    } else {
+      this.marks.push(mark);
+    }
+}//Создал метод addMark(mark), который при вызове добавляет студенту оценку mark в свойство (массив) marks объекта.
+
+Student.prototype.addMarks = function addMarks(mark1,mark2,mark3...) {
+  this.marks = (mark1,mark2,mark3...);
+}// тут не пойму, консоль выдает ошибку Uncaught SyntaxError: Unexpected token '...' 
+
+Student.prototype.getAverage = function getAverage() {
+  let res = 0;
+  for (let i = 0; i < this.marks.length; i++) {
+   res += this.marks[i];
+  }
+  return res/this.marks.length;
+}//5.Создал метод getAverage() который при вызове возвращает среднее арифметическое оценок студента.
+
+Student.prototype.exclude = function exclude(reason) {
+  delete Student.subject;
+  delete Student.marks;
+  this.excluded = reason;
+} //удалил свойства subject и marks и добавил свойство excluded со значением reason.
