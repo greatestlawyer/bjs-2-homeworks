cd6a1a5a82c4c4f2354bf78e1e9e0e151d1fa2fe
Артур Филонов <filonov.artur@bk.ru>
2022-08-25 16:12:48 +0300

    added tasks for check

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -13,3 +13,46 @@ function validateCount(value) {
       return error;
     }
 }
+
+//2 задача
+class Triangle{
+  constructor(
+    sideOne, 
+    sideTwo, 
+    sideThree) {
+    this.sideOne = sideOne,
+    this.sideTwo = sideTwo,
+    this.sideThree = sideThree;
+    if (sideOne + sideTwo < sideThree || 
+        sideOne + sideThree < sideTwo || 
+        sideTwo + sideThree < sideOne) {
+      throw new Error("Треугольник с такими сторонами не существует");
+      }  
+  }
+  getPerimeter() {
+    let perimeter = this.sideOne + this.sideTwo + this.sideThree;
+    return perimeter;
+  }
+  getArea() {
+    let halfPerimeter = 1/2 * (this.sideOne + this.sideTwo + this.sideThree);
+    let square = Math.sqrt(halfPerimeter * (halfPerimeter - this.sideOne)*(halfPerimeter - this.sideTwo)*(halfPerimeter - this.sideThree)).toFixed(3);
+    return square;
+  }
+}
+
+
+function getTriangle(sideOne, sideTwo, sideThree) {
+  try{
+    let triangle = new Triangle(sideOne, sideTwo, sideThree);
+    return triangle;
+  } catch(error) {
+      return {
+        getPerimeter() {
+          return "Ошибка! Треугольник не существует"
+        },
+        getArea() {
+          return "Ошибка! Треугольник не существует"
+        }
+      }
+    }
+}
